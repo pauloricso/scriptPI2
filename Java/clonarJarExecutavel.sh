e740d2c16232932066b314727b3f5e41e2b33b12
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-05-26 15:59:17 -0300

    novo clone jar executavel

--- Java/clonarJarExecutavel.sh
+++ Java/clonarJarExecutavel.sh
@@ -3,8 +3,8 @@
     echo "deseja clonar com CLI -- y/n?"
     read resposta
 
-    if ["$resposta" == "y"]
+    if ["$resposta" == "y"]; then
     git clone https://github.com/rogerio-alvesf/Aplicacao-Java-Projeto-PI-2Sem.git
     else 
-     
+    git clone https://github.com/GUILHERMEPSANTOS/Java-Gui-pi.git
     fi
