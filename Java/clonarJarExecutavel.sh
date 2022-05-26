ee06a101aa31432268b0fbc9d5a1fbd3f3ca7749
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-05-26 15:47:06 -0300

    tabelas com iserts

--- Java/clonarJarExecutavel.sh
+++ Java/clonarJarExecutavel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-    cd ..
+    echo "deseja clonar com CLI -- y/n?"
+    read resposta
+
+    if ["$resposta" == "y"]
     git clone https://github.com/rogerio-alvesf/Aplicacao-Java-Projeto-PI-2Sem.git
+    else 
+     
+    fi
